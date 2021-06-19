aecc70f4635de9c042971bd1ba418ec8346ad570
abhinavgeethan <abhinavgeethan@gmail.com>
2021-06-19 19:30:46 +0400

    Aiohttp Trial and Bug Fixes.
    1. Added VSCode specific files to gitignore.
    2. Status command now queries monitor asynchronously.

--- extensions/tester.py
+++ extensions/tester.py
@@ -2,8 +2,10 @@ import os
 import discord
 from discord.ext import commands
 from utils import send_embed
-from main import field
+from main import field,botName
 import requests
+import aiohttp
+from aiohttp import ClientSession
 
 class tester(commands.Cog):
   
@@ -18,26 +20,37 @@ class tester(commands.Cog):
   @commands.command(help='Checks server status.')
   async def status(self,ctx):
     msg=await ctx.send('Requesting for Status')
-    response = requests.request(
-      "POST",
-      "https://api.uptimerobot.com/v2/getMonitors",
-      data=f"api_key={os.environ['uptime_api_key']}&format=json&logs=0&response_times=1",
-      headers={
-      'content-type': "application/x-www-form-urlencoded",
-      'cache-control': "no-cache"
-      }
-    )
+    async with ClientSession() as session:
+      response = await session.request(
+        method="POST",
+        url="https://api.uptimerobot.com/v2/getMonitors",
+        data=f"api_key={os.environ['uptime_api_key']}&format=json&logs=0&response_times=1",
+        headers={
+        'content-type': "application/x-www-form-urlencoded",
+        'cache-control': "no-cache"
+        }
+      )
+      data=await response.json()
+    # response = requests.request(
+    #   "POST",
+    #   "https://api.uptimerobot.com/v2/getMonitors",
+    #   data=f"api_key={os.environ['uptime_api_key']}&format=json&logs=0&response_times=1",
+    #   headers={
+    #   'content-type': "application/x-www-form-urlencoded",
+    #   'cache-control': "no-cache"
+    #   }
+    # )
     try:
-      data=response.json()
+      # data=await response.json()
       status=data['monitors'][0]['status']
       response_time=data['monitors'][0]['response_times'][0]['value']
       avg=round(float(data['monitors'][0]['average_response_time']))
       if status==2:
-        await send_embed(ctx.channel,"Private Bot Server Status",'The Server is UP.',discord.Colour.green(),footer='clear',fields=[field("Discord API",f"{round(self.client.latency * 1000)} ms",True),field("Round-Trip Response",f"{response_time}"+(" ms" if response_time!='Unavailable' else '')+f"\nAverage: {avg}"+(" ms" if avg!='Unavailable' else ''),True)])
+        await send_embed(ctx.channel,f"{botName} Server Status",'The Server is UP.',discord.Colour.green(),footer='clear',fields=[field("Discord API",f"{round(self.client.latency * 1000)} ms",True),field("Round-Trip Response",f"{response_time}"+(" ms" if response_time!='Unavailable' else '')+f"\nAverage: {avg}"+(" ms" if avg!='Unavailable' else ''),True)])
       else:
-        await send_embed(ctx.channel,"Private Bot Server Status",'The monitoring server is malfunctioning. Private Bot is up and running.',discord.Colour.orange(),footer='clear')
+        await send_embed(ctx.channel,f"{botName} Server Status",'The monitoring server is malfunctioning. {botName} is up and running.',discord.Colour.orange(),footer='clear')
     except:
-      await send_embed(ctx.channel,"Private Bot Server Status",'The monitoring server is malfunctioning. Private Bot is up and running.',discord.Colour.orange(),footer='clear')
+      await send_embed(ctx.channel,f"{botName} Server Status",'The monitoring server is malfunctioning. {botName} is up and running.',discord.Colour.orange(),footer='clear')
     await msg.delete()
   
 
